02ababda100c7ede32ca230143cb51c2e767202f
Marco <marco.salman@gmail.com>
2022-07-15 22:18:24 -0400

    adding sessions, login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,17 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      render :new
+      redirect_to '/signup'
     end
-  end
-
+  end   
   private
 
   def user_params
-    params.require(:user).permit(
-      :first_name,
-      :last_name,
-      :email,
-      :password,
-      :password_confirmation
-    )
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
